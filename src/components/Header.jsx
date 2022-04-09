a56a8ee91f1438476c50451ab6ca7aca27f7afa2
mamun060 <obujraju420@gmail.com>
2022-04-10 00:53:01 +0600

    setup firebase & tailwind

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+    return ( 
+        <div className="w-screen h-screen flex flex-col text-blue-900">
+            Header components 
+        </div>
+     );
+}
+ 
+export default Header;
